301596883a4dd56f074086e807de2cc0b41c5aeb
SergFediaev <sergfediaev@gmail.com>
2024-02-17 14:50:52 +0300

    Домашка: спринт 01, неделя 3.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import './App.css';
-import React from "react";
-import {HW2} from "./HW/2/HW2";
+import './App.css'
+import React from 'react'
+import {HW3} from './HW/3/HW3'
 
 function App() {
     return (
         <div className="App">
             {/*<HW1/>*/}
-            <HW2/>
-            {/*<HW3 />*/}
+            {/*<HW2/>*/}
+            <HW3/>
             {/*<HW4 />*/}
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
